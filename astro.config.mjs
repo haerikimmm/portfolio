eb1113b03f2c80511656173717ee387127468a3d
Haeri Kim <haerikim@Haeriui-Macmini.local>
2026-03-13 00:04:58 +0900

    Add trailing slash for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portfolio',
   trailingSlash: 'always'
 });
